8c92810a1dbbd2c23078a96804c1b9b852525433
Guten Ye <ywzhaifei@gmail.com>
2025-10-14 13:23:51 +0800

    supercronic: disable inotify

--- supercronic/docker/entrypoint.sh
+++ supercronic/docker/entrypoint.sh
@@ -4,4 +4,4 @@ if [ -z "$(ls -A /config 2>/dev/null)" ]; then
   cp -r /config-template/* /config
 fi
 
-exec supercronic -json -inotify /config/crontab 2>&1 | logdy --config /config/logdy.config.json --ui-ip 0.0.0.0 --port 80 --no-analytics
+exec supercronic -json /config/crontab 2>&1 | logdy --config /config/logdy.config.json --ui-ip 0.0.0.0 --port 80 --no-analytics
