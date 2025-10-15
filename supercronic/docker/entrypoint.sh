216641c9cbe4e5f7d76e0630884348d390e81054
Guten Ye <ywzhaifei@gmail.com>
2025-10-15 21:22:59 +0800

    fix(supercronic): save logdy to right dir

--- supercronic/docker/entrypoint.sh
+++ supercronic/docker/entrypoint.sh
@@ -4,4 +4,7 @@ if [ -z "$(ls -A /config 2>/dev/null)" ]; then
   cp -r /config-template/* /config
 fi
 
+# logdy save config to current dir
+cd /config
+
 exec supercronic -json /config/crontab 2>&1 | logdy --config /config/logdy.config.json --ui-ip 0.0.0.0 --port 80 --no-analytics
